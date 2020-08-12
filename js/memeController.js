0680d02018f3aca8ce1129efc11d17c399dac19a
Tal Sasson <sassoontal@gmail.com>
2020-08-12 19:58:05 +0300

    add text input

--- js/memeController.js
+++ js/memeController.js
@@ -1,6 +1,56 @@
+var gCanvas;
+var gCtx;
+
+
 function init() {
     drawCanvas()
 }
 function onWriteText(text) {
     drawText(text)
+    
+}
+function onWriteButtomText(text) {
+    drawButtomText(text)
+    
+}
+function drawCanvas() {
+    gCanvas = document.querySelector('#canvas');;
+    gCtx = gCanvas.getContext('2d');
+    drawImgFromlocal()
+}
+
+function drawImgFromlocal() {
+    var img = new Image()
+    img.src = gImgs[0].url;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
+    }
+}
+
+function drawText(text) {
+    gCtx.lineWidth =  2;
+    gCtx.strokeStyle = 'black';
+    gCtx.fillStyle = 'white';
+    gCtx.font = '2.5rem impact';
+    gCtx.textBaseline = 'top';
+    
+    console.log(gCtx.textBaseline);
+    gCtx.textAlign = 'center';
+    gCtx.fillText(text, gCanvas.width/2 , 6, gCanvas.width);
+    gCtx.strokeText(text, gCanvas.width/2 , 6, gCanvas.width);
+}
+
+
+
+function drawButtomText(text) {
+    gCtx.lineWidth =  2;
+    gCtx.strokeStyle = 'black';
+    gCtx.fillStyle = 'white';
+    gCtx.font = '2.5rem impact';
+    gCtx.textBaseline = 'top';
+    
+    console.log(gCtx.font);
+    gCtx.textAlign = 'center';
+    gCtx.fillText(text, gCanvas.width/2 , gCanvas.height -50 , gCanvas.width);
+    gCtx.strokeText(text, gCanvas.width/2 , gCanvas.height -50, gCanvas.width);
 }
--- js/memeService.js
+++ js/memeService.js
@@ -1,6 +1,3 @@
-var gCanvas;
-var gCtx;
-
 
 var gImgs = [{ id: 1, url: './meme-imgs/1.jpg', keywords: ['happy'] }];
 var gMeme = {
@@ -14,34 +11,6 @@ var gMeme = {
             color: 'red'
         }]
 }
-
-
-function drawCanvas() {
-    gCanvas = document.querySelector('#canvas');;
-    gCtx = gCanvas.getContext('2d');
-    drawImgFromlocal()
-}
-
-function drawImgFromlocal() {
-    var img = new Image()
-    img.src = gImgs[0].url;
-    img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
-    }
-}
-
-function drawText(text, x=16, y=25) {
-    gCtx.lineWidth = '2';
-    gCtx.strokeStyle = 'black';
-    gCtx.fillStyle = 'white';
-    gCtx.font = '2.5rem impact';
-    gCtx.textBaseline = 'top';
-    console.log(gCtx.textBaseline);
-    gCtx.textAlign = 'center';
-    gCtx.fillText(text, gCanvas.width/2 , 0, gCanvas.width);
-    gCtx.strokeText(text, x, y);
-}
-
 
 
 
